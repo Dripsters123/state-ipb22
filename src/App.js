3fa244b56ac83bf86025a3fe37dd420cf66c40bb
Your Name <you@example.com>
2023-10-19 09:49:12 +0300

    ratioChange

--- src/App.js
+++ src/App.js
@@ -3,22 +3,25 @@ import { useState } from "react";
 function App() {
 
   const [result, setResult] = useState(0);
+  const [ratioChange, setRatioChange] = useState(1);
 
 function addRatio(){
-  setResult(result + 1);
+  setResult(result + parseInt(ratioChange));
   console.log("x ir "+ result)
 }
 function minusRatio(){
-  setResult(result - 1);
-  console.log("funkcija izsaukta");
-  console.log("x ir "+ result)
+  setResult(result - ratioChange);
+}
+function handleRatioChange(kaķēns){
+  setRatioChange(kaķēns.target.value);
 }
 
  return (
 <div className="App">
-  <input></input>
-  <button onClick={addRatio}>Button +1</button>
-  <button onClick={minusRatio}>Button - 1</button>
+  <input type="number" value={ratioChange} onChange={handleRatioChange}></input>
+  <br/>
+  <button onClick={addRatio}>Button + {ratioChange}</button>
+  <button onClick={minusRatio}>Button - {ratioChange}</button>
   <h1>{result}</h1>
 </div>
  );
