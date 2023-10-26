43b40035de07688ebd29a4debf6886409b30a535
Dripsters123 <https://github.com/Dripsters123/Dripsters123.github.io>
2023-10-26 21:32:14 +0300

    majasdarbs

--- src/App.js
+++ src/App.js
@@ -1,32 +1,44 @@
 import Counter from "./Counter";
 import Check from "./Check";
 import Hello from "./Hello";
+import LabelCheck from "./LabelCheck";
 
 function App() {
-  const ratios = [7,43,101];
-  const ratiosJSX = ratios.map((theRatios, indexx)=>{
-    return <Counter defaultRatios={theRatios} key = {indexx}/>
-  })
+  const ratios = [7, 43, 101];
+  const ratiosJSX = ratios.map((theRatios, indexx) => {
+    return <Counter defaultRatios={theRatios} key={indexx} />;
+  });
 
-const vardi = ["Kristapus","Mini P.E.K.K.A","Ositis"];
-const vardiJSX = vardi.map((theName, index) =>{
-  return <Hello defaultName={theName} key={index}/>
-  
-})
-const check = [true,true,false];
-const checkJSX = check.map((theCheck, indexxx)=>{
- return <Check checkValue={theCheck} key={indexxx}/>
-})
+  const vardi = ["Kristapus", "Mini P.E.K.K.A", "Ositis"];
+  const vardiJSX = vardi.map((theName, index) => {
+    return <Hello defaultName={theName} key={index} />;
+  });
+  const check = [true, true, false];
+  const checkJSX = check.map((theCheck, indexxx) => {
+    return <Check checkValue={theCheck} key={indexxx} />;
+  });
+  const labelInputs = [
+    { label: "Agree to the Policy", checked: true },
+    { label: "Receive Offers", checked: false },
+  ];
+  const labelCheckJSX = labelInputs.map((lableInput, index) => {
+    return (
+      <LabelCheck
+        key={index}
+        label={lableInput.label}
+        checked={lableInput.checked}
+      />
+    );
+  });
 
-return(
-  <div>
-    
-  {ratiosJSX}
-  {vardiJSX}
-  {checkJSX}
- </div>
-  
-)
+  return (
+    <div>
+      {ratiosJSX}
+      {vardiJSX}
+      {checkJSX}
+      {labelCheckJSX}
+    </div>
+  );
 }
 
-export default App; 
+export default App;
